28828756272329180c1ef5bf57904995dc8ff0ec
anthonypitts2022 <anthony.pitts@columbia.edu>
2021-03-24 20:10:02 -0400

    Fixed pow function to include std math library. All test pass.

--- testall.sh
+++ testall.sh
@@ -96,7 +96,7 @@ Check() {
     generatedfiles="$generatedfiles ${basename}.ll ${basename}.s ${basename}.exe ${basename}.out" &&
     Run "$POLYWIZ" "$1" ">" "${basename}.ll" &&
     Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
-    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "printbig.o" &&
+    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "printbig.o -lm" &&
     Run "./${basename}.exe" > "${basename}.out" &&
     Compare ${basename}.out ${reffile}.out ${basename}.diff
 
